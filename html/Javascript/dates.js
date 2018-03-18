4830017cb10a60a6afaa6d9feb76a71add418054
andrushka84 <palici.andreea@gmail.com>
2018-03-18 21:53:55 +0200

    Am pus o versiune din tema ca sa nu uit.

---
+++ html/Javascript/dates.js
@@ -0,0 +1,30 @@
+function myFunction(){
+    var d1 = new Date();
+    var dt1 = d1.toDateString();
+    document.getElementById("d1").innerHTML=dt1;
+
+}
+
+//var today = d1.getDate();
+//var month = d1.getMonth();
+//var year = d1.getFullYear();
+////
+//var d2 = d1.getDate()+1;
+//var d3 = d1.getDate()+2;
+//var d4 = d1.getDate()+3;
+//var d5 = d1.getDate()+4;
+//var d6 = d1.getDate()+5;
+
+//var day1 = today+"/"+month+"/"+year;
+//var day2 = d2+"/"+month+"/"+year;
+//var day3 = d3+"/"+month+"/"+year;
+//var day4 = d4+"/"+month+"/"+year;
+//var day5 = d5+"/"+month+"/"+year;
+//var day6 = d6+"/"+month+"/"+year;
+
+//var zi = d1.getTime(year, month, today);
+//var zi0 = zi - d1.getTime(year, month, d1.getDate-1)
+//
+//if zi-zi0 < 60*60*24*1000 {
+//}
+
--- html/Javascript/weather.js
+++ html/Javascript/weather.js
@@ -8,8 +8,8 @@ function city(){
 
 
     var request = new XMLHttpRequest();
-    // Ma intereseaza serverul sa fi terminat de incarcat (ready state = 4) si
-    // Si sa nu dea eroare; status = 200 inseamna ca e OK.
+    // Ma intereseaza serverul sa fi terminat de incarcat (ready state === 4) si
+    // Si sa nu dea eroare; status === 200 inseamna ca e OK.
     // Vreau sa transform variabila vreme in obiect JSON si utilizez JSON parse pt asta
     request.onreadystatechange = function(){
         if(this.readyState===4 && this.status===200) {
@@ -18,19 +18,18 @@ function city(){
             // Astea sunt variabilele care imi trebuie;
             // Dau click pe adresa de net de la request.open () sa-mi afiseze API in format text 
             
-           
             var descriere = vreme.weather[0].description;
             var umiditate = vreme.main.humidity;
             var presiune = vreme.main.pressure;
             var temperatura = vreme.main.temp;
             var minima=vreme.main.temp_min;
             var maxima = vreme.main.temp_max;
-            // *********************************************************************************
             var latitudine = vreme.coord.lat ;
             var longitudine = vreme.coord.lon ;
             var icon = "http://openweathermap.org/img/w/"+vreme.weather[0].icon +".png";
              
             // *********************************************************************************
+            // Initializez Google Maps
             initMap(latitudine, longitudine);
 
             // *********************************************************************************
@@ -43,33 +42,113 @@ function city(){
         document.getElementById("maxima").innerHTML = maxima;
         document.getElementById("icon").src = icon;
         }           
-
-        
 }
 
 request.open("GET", url, true);
 request.send();
 }
 
-// "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=Paris"
-
-
 // **************************************************************************************************************
-// function prognoza() {
 
-//     if (document.getElementById("nume").value !== null){
-//         var oras = document.getElementById("nume").value;
-//         var url = 'https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=' + oras;
-//     }   
+function prognoza() {
 
-// var forecast = new XMLHttpRequest();
+    if (document.getElementById("nume").value !== null){
+        var oras = document.getElementById("nume").value;
+        var url = 'https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=' + oras;
+    }   
+
+        // Fac capul de tabel cu datele alea albastre
+        // new Date () imi arata data de azi in format full date (data, ziua, ora, minut, secunda)
+        // toDateString = imi extrage ziua si data
+        var d1 = new Date();
+        var dt1 = d1.toDateString();
+        document.getElementById("d1").innerHTML=dt1;
+
+        var month = d1.getMonth();
+        var year = d1.getFullYear();
     
-// forecast.onreadystatechange = function(){
-//     if(this.readyState===4 && this.status===200) {
-//         var vreme = JSON.parse(this.responseText);
-//         console.log(vreme);     
-// }
+        var d2 = new Date(year, month, d1.getDate()+1);
+        var dt2 = d2.toDateString();
+        document.getElementById("d2").innerHTML=dt2;
+       
+        var d3 = new Date(year, month, d1.getDate()+2);
+        var dt3 = d3.toDateString();
+        document.getElementById("d3").innerHTML=dt3;
+    
+        var d4 = new Date(year, month, d1.getDate()+3);
+        var dt4 = d4.toDateString();
+        document.getElementById("d4").innerHTML=dt4;
+    
+        var d5 = new Date(year, month, d1.getDate()+4);
+        var dt5 = d5.toDateString();
+        document.getElementById("d5").innerHTML=dt5;
+    
+        var d6 = new Date(year, month, d1.getDate()+5);
+        var dt6 = d6.toDateString();
+        document.getElementById("d6").innerHTML=dt6;
 
-// }
+        // Dau XMLHttpRequest
 
-// }
+        var prog = new XMLHttpRequest();
+        
+        prog.onreadystatechange = function(){
+            if(this.readyState===4 && this.status===200) {
+                var vreme = JSON.parse(this.responseText);
+                console.log(vreme); 
+    
+        for (var i = 0; i<vreme.list.length; i++){
+        var col1= vreme.list[i].dt_txt;
+
+    }
+
+    // Calculez cate ore sa-mi afiseze in prima zi
+    
+    var b= d1.getHours();
+
+               if (((24-b)/3)<=8 && ((24-b)/3)>7) {
+            var y = 8;
+        } else if (((24-b)/3)<=7 && ((24-b)/3)>6) {
+            var y = 7
+        } else if (((24-b)/3)<=6 && ((24-b)/3)>5) {
+            var y = 6;
+        } else if (((24-b)/3)<=5 && ((24-b)/3)>4) {
+            var y = 5;
+        } else if (((24-b)/3)<=4 && ((24-b)/3)>3) {
+            var y = 4;
+        } else if (((24-b)/3)<=3 && ((24-b)/3)>2) {
+            var y = 3;
+        } else if (((24-b)/3)<=2 && ((24-b)/3)>1) {
+            var y = 2;
+        } else {var y = 1}
+
+       console.log(y);
+
+        function prima(){
+            for (var j=1; j<y; j++){
+
+                var azi = vreme.list[j].dt_txt;
+                console.log(azi);
+                }
+        }
+        var m = vreme.list.length - y - (8*4)+1;
+        console.log(m);
+        
+       
+        // function ultima() {
+        //     for (var k=1; k>39-m; k++){
+        //         var last = vreme.list[k].dt_txt;
+        //         console.log(last);
+        //         }
+        // }
+       
+
+        }
+    } 
+
+
+prog.open("GET", url, true);
+prog.send();
+
+}
+
+
