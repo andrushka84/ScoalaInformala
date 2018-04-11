ddc0cea797ae916abcaf465cda321dcc55051ef0
andrushka84 <palici.andreea@gmail.com>
2018-04-12 02:02:36 +0300

    a

--- html/Javascript/meniu app/delete.js
+++ html/Javascript/meniu app/delete.js
@@ -5,14 +5,14 @@ function stergePreparat(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            drawPreparateAdmin();
+            window.location = "admin.html";
         }
     };
     var id =window.location.search.substring(4);
     
     // *******************************************************************************************
-    var preparat = listaPreparate.nume;
-    document.getElementById("preparat").innerHTML = preparat;
+    // var preparat = listaPreparate.nume;
+    // document.getElementById("preparat").innerHTML = preparat;
     // *******************************************************************************************
     
     xhttp.open("DELETE", "https://menu-83f09.firebaseio.com/menu/"+id+"/.json", true);
@@ -20,6 +20,7 @@ function stergePreparat(){
 
 }
 
+    // de vazut cum fac sa-mi arate numele preparatului
+    // la var preparat ... imi da eroare loop infinit !!!!!!!!!!!!!!!!!!
     //am pus window.location = "admin.html" in loc de drawPreparateAdmin()
-
-
+    
