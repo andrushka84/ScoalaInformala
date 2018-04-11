e06323690331e569cb4ed86fc3871cfb5108713a
andrushka84 <palici.andreea@gmail.com>
2018-04-12 00:44:39 +0300

    b

--- html/Javascript/meniu app/delete.js
+++ html/Javascript/meniu app/delete.js
@@ -1,19 +1,25 @@
+var listaPreparate=[];
 
 function stergePreparat(){
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            window.location = "admin.html";
+            drawPreparateAdmin();
         }
     };
     var id =window.location.search.substring(4);
-
-    xhttp.open("DELETE", "https://menu-83f09.firebaseio.com/menu"+ id + "/.json", true);
+    
+    // *******************************************************************************************
+    var preparat = listaPreparate.nume;
+    document.getElementById("preparat").innerHTML = preparat;
+    // *******************************************************************************************
+    
+    xhttp.open("DELETE", "https://menu-83f09.firebaseio.com/menu/"+id+"/.json", true);
     xhttp.send();
 
 }
 
-// am pus window.location = "admin.html" in loc de drawPreparateAdmin()
+    //am pus window.location = "admin.html" in loc de drawPreparateAdmin()
 
 
