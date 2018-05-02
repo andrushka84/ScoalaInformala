6c71522807bfe9d7e8ebd71211590d00f1a3ed8e
andrushka84 <palici.andreea@gmail.com>
2018-05-02 14:35:14 +0300

    provizorii

--- html/Javascript/magazin online/cart3.js
+++ html/Javascript/magazin online/cart3.js
@@ -1,7 +1,7 @@
 
 var subtotal = 0;
 
-
+// Adauga primul produs in cos si memoreaza cosul cand adaug mai multe produse
 function aduProdusul() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -31,73 +31,60 @@ function aduProdusul() {
 
             // *****************************************************************************************
             memoreazaCosul(event);
-
-            // *****************************************************************************************
+            // // *****************************************************************************************
             deseneazaCosul();
         }
     };
 
-    var id =window.location.search.substring(4);
-   
+    var id = window.location.search.substring(4);
 
     xhttp.open("GET", "https://magazin-online-64a15.firebaseio.com/"+id+"/.json", true);
     xhttp.send();
 }
 
+// Sterge un produs din cos
 
 function stergeProdusDinCos() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+        //   deseneazaCosul();
 
-     subtotal = 0;
-     document.getElementById("subtotal").innerHTML = subtotal;
-
-     var tva = 0.19 * subtotal;
-     document.getElementById("tva").innerHTML = tva;
-     
-     var total = subtotal + tva;
-     document.getElementById("total").innerHTML = total;
-
-     document.getElementById("loading").style.display = "none";
-     
+    }
 }
 
+    var id =window.location.search.substring(4);
 
-function actualizeazaCosul(){
+    xhttp.open("DELETE", "https://shopping-cart-magazin-online.firebaseio.com/"+id+"/.json", true);
+    xhttp.send();
+}
 
+// Actualizeaza cantitatea din cos; ar trebui sa-mi calculeze si subtotalul 
+
+function actualizeazaCosul(elem){
+
+    elem.value = 1;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-
-            listaProduse = JSON.parse(this.responseText);
-
-            var poza = listaProduse.imagine;
-            document.getElementById("poza").src = poza;
-
-            var produs = listaProduse.titlu;
-            document.getElementById("produs").innerHTML = produs;
-
-            var pret = parseInt(listaProduse.pret);
-            document.getElementById("pret").innerHTML = pret;
-
-            var cantitate= parseInt(document.getElementById("cantitate").value);
-
-            subtotal = pret * cantitate;
-            document.getElementById("subtotal").innerHTML = subtotal;
-
-            var tva = 0.19 * subtotal;
-            document.getElementById("tva").innerHTML = tva;
-
-            var total = subtotal + tva ;
-            document.getElementById("total").innerHTML = total;
-
         }
+
+         listaProduse = JSON.parse(this.responseText);
     };
 
     var id =window.location.search.substring(4);
-   
-    xhttp.open("GET", "https://magazin-online-64a15.firebaseio.com/"+id+"/.json", true);
-    xhttp.send();
+
+    xhttp.open("PUT", "https://shopping-cart-magazin-online.firebaseio.com/" +id +".json", true);
+
+    xhttp.send(JSON.stringify({
+
+        cantitate: listaProduse.cantitate,
+        
+    }));
+
 }
 
+// Memoreaza cosul atunci cand adaug mai mult de un produs
 
     function memoreazaCosul(event){
         event.preventDefault();
@@ -114,7 +101,7 @@ function actualizeazaCosul(){
                 poza: document.querySelector("#poza").src,
                 produs: document.querySelector("#produs").innerHTML,
                 pret: document.querySelector("#pret").innerHTML,
-                cantitate: document.querySelector("#cantitate").value,
+                cantitate: parseInt(document.querySelector("#cantitate").value),
                 subtotal: document.querySelector("#subtotal").innerHTML,
                 tva: document.querySelector("#tva").innerHTML,
                 total: document.querySelector("#total").innerHTML,
@@ -124,65 +111,59 @@ function actualizeazaCosul(){
         }
     
 
-// DE TERMINAT ****************************************************************************************************
-function deseneazaCosul(){
+// Deseneaza tabelul cu produsele din cos
 
-var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+function deseneazaCosul() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            var tabel = document.querySelector("#cart tbody");
+            var str = "";
+            var listaProduse = JSON.parse(this.responseText);
 
-            listaProduse = JSON.parse(this.responseText);
-
-            // *****************************************************************************************
-            var tabel=document.querySelector("#cart tbody");
-			var str="";
-
-			for(var i in listaProduse){
-				
+            for (var i in listaProduse) {
                 var rand = `<tr>
 
-                <td>
-                        <div class="col-sm-2">
-                                <img src="" alt="..." class="img-responsive" id="poza"/>
-                                </div>
-                </td>
+        <td data-th="">
+            <div class="col-sm-2">
+                <img src=${listaProduse[i].poza} alt="" class="img-responsive" />
+            </div>
+        </td>
 
-                <td data-th="Product">
-                    <div class="row">
-                        <div class="col-sm-10 " id="produs"></div>
-                        
-                    </div>
-                </td>
-               
-                <td data-th="Price" id="pret"></td>
-                <td data-th="Quantity" >
-                    <input type="number" class="form-control text-center" value="1" id="cantitate">
-                </td>
-                <td data-th="Subtotal" class="text-center" id="subtotal"></td>
-                <td class="actions" data-th="">
-                    <button class="btn btn-info btn-sm" id="refresh" ><i class="fas fa-sync-alt" onclick="actualizeazaCosul()"></i></button>
-                    <button class="btn btn-danger btn-sm" id="delete" onclick="stergeProdusDinCos()"><i class="fas fa-trash"></i></button>								
-                </td>
+        <td data-th="Product">
+                <div class="col-sm-10">${listaProduse[i].produs}</div>
+        </td>
 
-               <tr/> `
+        <td data-th="Price">${listaProduse[i].pret}</td>
 
-				str +=rand;
-			}
-			console.log(str);
-			tabel.innerHTML=str;
-		}
-	};
-            // *****************************************************************************************
+        <td data-th="Quantity">
+            <input type="number" class="form-control text-center" value="${listaProduse[i].cantitate}" onchange="actualizeazaCosul(elem);" />
+        </td>
 
-        
-    
+        <td data-th="Subtotal" class="text-center">${listaProduse[i].subtotal}</td>
+
+        <td class="actions" data-th="">
+            <button class="btn btn-info btn-sm" id="refresh">
+                <i class="fas fa-sync-alt" onclick="actualizeazaCosul(elem)"></i>
+            </button>
+            <button class="btn btn-danger btn-sm" id="delete" onclick="stergeProdusDinCos()">
+                <i class="fas fa-trash"></i>
+            </button>
+        </td>
+        </tr> `
+                str += rand;
+            }
+            console.log(str);
+            tabel.innerHTML = str;
+        }
+    };
 
     xhttp.open("GET", "https://shopping-cart-magazin-online.firebaseio.com/.json", true);
     xhttp.send();
-
-
 }
 
+
+// Sterge cosul
 
 function stergeCosul(){
 
--- html/Javascript/magazin online/checkout.js
+++ html/Javascript/magazin online/checkout.js
@@ -6,3 +6,30 @@ function numarComanda(){
 }
 
 
+function iaComanda(){
+    var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var tabel = document.querySelector("#cart tbody");
+                    var str = "";
+                    var listaProduse = JSON.parse(this.responseText);
+
+                    for (var i in listaProduse) {
+                        var rand = `<tr>
+
+                       <td> <img src= "${listaProduse[i].poza}"/> </td>
+                       <td> ${listaProduse[i].produs} </td>
+                       <td>${listaProduse[i].pret}</td>
+                       <td>${listaProduse[i].cantitate}</td>
+                       <td>${listaProduse[i].subtotal}</td>
+
+                       </tr>`
+                    }
+
+                }
+
+            }
+
+            xhttp.open("GET", "https://shopping-cart-magazin-online.firebaseio.com/.json", true);
+            xhttp.send();
+        }
