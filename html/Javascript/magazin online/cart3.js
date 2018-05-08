545e29e221233c0c930314ff786c476ae7defe5c
andrushka84 <palici.andreea@gmail.com>
2018-05-08 21:05:43 +0300

    bun

--- html/Javascript/magazin online/cart3.js
+++ html/Javascript/magazin online/cart3.js
@@ -51,7 +51,7 @@ function stergeProdusDinCos() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-
+            
               deseneazaCosul();
         }
     }
